6b257a5126bad769218d45c1f95859ae8e8778e5
AgusCF <acardetti295@alumnos.iua.edu.ar>
2025-09-13 18:26:21 -0300

    ProyectoBase
    Creacion del proyecto base

---
+++ PrimerProyecto/primerproyecto/src/test/java/com/primerproyecto/AppTest.java
@@ -0,0 +1,17 @@
+package com.primerproyecto;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
+
